c99ff5b47155a1321b9150158785d59821e4fd3f
SUVOJIT KAR <skar@hpe.com>
2022-06-21 03:10:52 +0530

    added

--- src/App.js
+++ src/App.js
@@ -40,59 +40,54 @@ function App() {
 
   return (
     <div className="App">
-      
-      
+      <nav> Suvojit </nav>
+
+      <div className="card">
       <h1> sreeparna chanda</h1>
-      <div className="images">
-        <img src="/Assets/sreeparna/1.png"/>
-        <img src="/Assets/sreeparna/2.png" />
-        <img src="/Assets/sreeparna/3.png" />
-        <img src="/Assets/sreeparna/4.png" />
-      </div>
-
-      <br></br>
-
-      <div className="details">
-        {Object.keys(sreeparna).map(function(keyName, keyIndex) {
-          return (
-              <div key={keyName}>
-              {keyName}: {sreeparna[keyName]}
-              </div>
-          )
-        })}
-      </div>
-
-      <br></br>
-
-      <hr />
-
-      <br></br>
-
-      <h1> Ananya sinha roy </h1>
-      <div className="images">
-        <img src="/Assets/ananya/1.png"/>
-        <img src="/Assets/ananya/2.png"/>
-        <img src="/Assets/ananya/3.png"/>
-        <img src="/Assets/ananya/4.png"/>
-        <img src="/Assets/ananya/5.png"/>
-        <img src="/Assets/ananya/6.png"/>
-        <img src="/Assets/ananya/7.png"/>
-      </div>
-
-      <br></br>
-
-      <div className="details">
-        {Object.keys(ananya).map(function(keyName, keyIndex) {
-          return (
-              <div key={keyName}>
-              {keyName}: {ananya[keyName]}
-              </div>
-          )
-        })}
+        <div className="images">
+          <img src="https://suvojitkar.github.io/test/Assets/sreeparna/1.png" alt="img"/>
+          <img src="https://suvojitkar.github.io/test/Assets/sreeparna/2.png" alt="img"/>
+          <img src="https://suvojitkar.github.io/test/Assets/sreeparna/3.png" alt="img"/>
+          <img src="https://suvojitkar.github.io/test/Assets/sreeparna/4.png" alt="img"/>
+        </div>
+        <br></br>
+        <div className="details">
+          <br></br>
+          {Object.keys(sreeparna).map(function(keyName, keyIndex) {
+            return (
+                <div key={keyName}>
+                {keyName}: {sreeparna[keyName]}
+                </div>
+            )
+          })}
+        </div>
+        <br></br>
       </div>
       
-
-
+      <br></br>
+      
+      <div className="card">
+      <h1> Ananya sinha roy </h1>
+        <div className="images">
+          <img src="/test/Assets/ananya/1.png" alt="img"/>
+          <img src="/test/Assets/ananya/2.png" alt="img"/>
+          <img src="/test/Assets/ananya/3.png" alt="img"/>
+          <img src="/test/Assets/ananya/4.png" alt="img"/>
+          <img src="/test/Assets/ananya/5.png" alt="img"/>
+          <img src="/test/Assets/ananya/6.png" alt="img"/>
+          <img src="/test/Assets/ananya/7.png" alt="img"/>
+        </div>
+        <br></br>
+        <div className="details">
+          {Object.keys(ananya).map(function(keyName, keyIndex) {
+            return (
+                <div key={keyName}>
+                {keyName}: {ananya[keyName]}
+                </div>
+            )
+          })}
+        </div>
+      </div>
       
     </div>
   );
